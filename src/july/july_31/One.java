60de8f04ca780d94779a7a0d83a9987826ceeadf
kshitij varshney <kshitijvarshney92@gmail.com>
2021-07-31 06:52:36 +0530

    create One.java

---
+++ src/july/july_31/One.java
@@ -0,0 +1,12 @@
+/*   Created by IntelliJ IDEA.
+ *   Author: Kshitij Varshney (kshitijvarshne1)
+ *   Date: 31-Jul-21
+ *   Time: 6:52 AM
+ *   File: One.java
+ */
+
+package july.july_31;
+
+public class One {
+}
+
