b7b7b62c5a227b81074ecdc011d7e3476505f2c7
kshitij varshney <kshitijvarshney92@gmail.com>
2021-08-03 19:10:34 +0530

    create Main.java

--- src/august/aug03_21/Main.java
+++ src/august/aug03_21/Main.java
@@ -1,12 +1,12 @@
 /*   Created by IntelliJ IDEA.
  *   Author: Kshitij Varshney (kshitijvarshne1)
  *   Date: 03-Aug-21
- *   Time: 7:05 PM
- *   File: BinarySearchTree.java
+ *   Time: 7:10 PM
+ *   File: Main.java
  */
 
 package august.aug03_21;
 
-public class BinarySearchTree {
+public class Main {
 }
 
