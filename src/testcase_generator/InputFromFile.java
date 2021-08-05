87c73f18ad242c320926258582372f7c04f649b6
kshitij varshney <kshitijvarshney92@gmail.com>
2021-08-05 16:57:00 +0530

    create InputFromFile.java

---
+++ src/testcase_generator/InputFromFile.java
@@ -0,0 +1,12 @@
+/*   Created by IntelliJ IDEA.
+ *   Author: Kshitij Varshney (kshitijvarshne1)
+ *   Date: 05-Aug-21
+ *   Time: 4:56 PM
+ *   File: InputFromFile.java
+ */
+
+package testcase_generator;
+
+public class InputFromFile {
+}
+
