61b211120c8b3937fd77b6f7e7b18f1ae679fd83
kshitij varshney <kshitijvarshney92@gmail.com>
2021-08-05 16:25:59 +0530

    create Cookie.java

---
+++ src/august/aug08_21/Cookie.java
@@ -0,0 +1,12 @@
+/*   Created by IntelliJ IDEA.
+ *   Author: Kshitij Varshney (kshitijvarshne1)
+ *   Date: 05-Aug-21
+ *   Time: 4:25 PM
+ *   File: Cookies.java
+ */
+
+package august.aug08_21;
+
+public class Cookie {
+}
+
