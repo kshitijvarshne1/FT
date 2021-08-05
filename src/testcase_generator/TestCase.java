674966f1d86c925348248d89f081708990242a99
kshitij varshney <kshitijvarshney92@gmail.com>
2021-08-05 16:56:37 +0530

    final commit

--- src/testcase_generator/TestCase.java
+++ src/testcase_generator/TestCase.java
@@ -7,6 +7,37 @@
 
 package testcase_generator;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+
 public class TestCase {
+    public static void main(String[] args) throws IOException {
+        File file = new File("C:\\Users\\kshitij varshney\\IdeaProjects\\FT\\src\\testcase_generator\\inp.txt");
+        FileWriter fw = new FileWriter(file);
+        PrintWriter pw = new PrintWriter(fw);
+        int minT = 1;
+        int maxT = 10;
+        int rangeT = maxT - minT + 1;
+        int t = (int) (Math.random() * rangeT) + minT;
+        pw.println(t);
+        for (int i = 0; i < t; i++) {
+            int minN = 5;
+            int maxN = 100;
+            int rangeN = maxN - minN + 1;
+            int n = (int) (Math.random() * rangeN) + minN;
+            pw.println(n);
+            for (int i1 = 0; i1 < n; i1++) {
+                int ele = (int) (Math.random() * 100) + 1;
+                pw.print(ele+" ");
+            }
+            pw.println();
+            int k = (int) (Math.random() * 10) + 1;
+            pw.println(k);
+        }
+        pw.close();
+
+    }
 }
 
