588effa21e2e065c9e77c2aa108acd6a24f9731d
kshitij varshney <kshitijvarshney92@gmail.com>
2021-08-05 16:38:23 +0530

    create TestCase.java

---
+++ src/testcase_generator/TestCase.java
@@ -0,0 +1,12 @@
+/*   Created by IntelliJ IDEA.
+ *   Author: Kshitij Varshney (kshitijvarshne1)
+ *   Date: 05-Aug-21
+ *   Time: 4:38 PM
+ *   File: TestCase.java
+ */
+
+package testcase_generator;
+
+public class TestCase {
+}
+
