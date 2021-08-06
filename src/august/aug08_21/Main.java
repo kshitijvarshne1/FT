a4f9cd4c7ce85dd01fbd25a750edb3cf36441a48
kshitij varshney <kshitijvarshney92@gmail.com>
2021-08-06 16:07:27 +0530

    final commit

--- src/august/aug08_21/Main.java
+++ src/august/aug08_21/Main.java
@@ -7,6 +7,64 @@
 
 package august.aug08_21;
 
-public class Main {
-}
+import java.util.Scanner;
 
+/* Name of the class has to be "Main" only if the class is public. */
+class Main {
+    public static void main(String[] args) throws java.lang.Exception {
+        Scanner sc = new Scanner(System.in);
+
+        int t = sc.nextInt();
+        while (t-- > 0) {
+            int n = sc.nextInt();
+            int[] arr = new int[n];
+            for (int i = 0; i < n; i++) {
+                arr[i] = sc.nextInt();
+            }
+            int k = sc.nextInt();
+            System.out.println(cookieDist(arr, k));
+        }
+    }
+
+    static boolean isValid(int[] a, int n, int mid, int k) {
+        int cokkie = 1;
+        int sum = 0;
+        for (int i = 0; i < n; i++) {
+            sum += a[i];
+            if (sum > mid) {
+                cokkie++;
+                sum = a[i];
+            }
+            if (cokkie > k) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    static int cookieDist(int[] a, int k) {
+        int n = a.length;
+        if (n < k) {
+            return -1;
+        }
+        int sum = 0;
+        int maxi = -1;
+        for (int i = 0; i < n; i++) {
+            maxi = Math.max(maxi, a[i]);
+            sum += a[i];
+        }
+        int s = maxi;
+        int e = sum;
+        int res = -1;
+        while (s <= e) {
+            int mid = s + (e - s) / 2;
+            if (isValid(a, n, mid, k) == true) {
+                res = mid;
+                e = mid - 1;
+            } else {
+                s = mid + 1;
+            }
+        }
+        return res;
+    }
+}
