d9c8deac9b3c2b45b5451ed222289a8f71887aa9
kshitij varshney <kshitijvarshney92@gmail.com>
2021-08-11 22:20:07 +0530

    create Solution.java

---
+++ src/august/aug09_21/Solution.java
@@ -0,0 +1,12 @@
+/*   Created by IntelliJ IDEA.
+ *   Author: Kshitij Varshney (kshitijvarshne1)
+ *   Date: 11-Aug-21
+ *   Time: 10:19 PM
+ *   File: Solution.java
+ */
+
+package august.aug09_21;
+
+public class Solution {
+}
+
