b8c6432bdc3bcbacc21f21e4907781887189c600
kshitij varshney <kshitijvarshney92@gmail.com>
2021-08-12 20:11:18 +0530

    final commit

--- src/august/recursion/PrintNumber.java
+++ src/august/recursion/PrintNumber.java
@@ -8,5 +8,35 @@
 package august.recursion;
 
 public class PrintNumber {
+    public static void main(String[] args) {
+        // Print the numbers from 1 to n using recursion
+        int n= 50;
+        printNumbersInIncreasingOrder(n);
+        System.out.println();
+        // Print the numbers from n to 1 using recursion
+        printNumbersInDecreasingOrder(n);
+    }
+
+
+    private static void printNumbersInIncreasingOrder(int n) {
+        // Base Condition
+        if(n<1){
+            return ;
+        }
+        // recursively call the function
+        printNumbersInIncreasingOrder(n-1);
+        // print the number
+        System.out.print(n+" ");
+    }
+    private static void printNumbersInDecreasingOrder(int n) {
+        // Base Condition
+        if(n<1){
+            return ;
+        }
+        // print the number
+        System.out.print(n+" ");
+        // recursively call the function
+        printNumbersInDecreasingOrder(n-1);
+    }
 }
 
