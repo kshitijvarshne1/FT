4c577c4784c5e1ba00606030a306a95a8d2a1e15
kshitij varshney <kshitijvarshney92@gmail.com>
2021-08-12 20:12:17 +0530

    create Factorial.java

---
+++ src/august/recursion/Factorial.java
@@ -0,0 +1,12 @@
+/*   Created by IntelliJ IDEA.
+ *   Author: Kshitij Varshney (kshitijvarshne1)
+ *   Date: 12-Aug-21
+ *   Time: 8:12 PM
+ *   File: Factorial.java
+ */
+
+package august.recursion;
+
+public class Factorial {
+}
+
