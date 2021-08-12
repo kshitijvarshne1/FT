b805b3306b636e47c57eca2278dca18f4641c9f7
kshitij varshney <kshitijvarshney92@gmail.com>
2021-08-12 22:48:59 +0530

    final commit

--- src/august/recursion/Factorial.java
+++ src/august/recursion/Factorial.java
@@ -8,5 +8,23 @@
 package august.recursion;
 
 public class Factorial {
+    public static void main(String[] args) {
+        // find factorial of a number
+        int n1 = 1;
+        System.out.println("factorial of " + n1 + " is : " + factorial(n1));
+        int n2 = 5;
+        System.out.println("factorial of " + n2 + " is : " + factorial(n2));
+        int n3 = 11;
+        System.out.println("factorial of " + n3 + " is : " + factorial(n3));
+    }
+
+    private static int factorial(int n) {
+        //base condition
+        if(n<=1){
+            return 1;
+        }
+        // recursively call the method with n-1
+        return n*factorial(n-1);
+    }
 }
 
