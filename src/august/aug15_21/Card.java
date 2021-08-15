a2d4b05b67a810345653767a5ee756acee9620e9
kshitij varshney <kshitijvarshney92@gmail.com>
2021-08-15 13:28:26 +0530

    create Card.java

---
+++ src/august/aug15_21/Card.java
@@ -0,0 +1,12 @@
+/*   Created by IntelliJ IDEA.
+ *   Author: Kshitij Varshney (kshitijvarshne1)
+ *   Date: 15-Aug-21
+ *   Time: 1:28 PM
+ *   File: Card.java
+ */
+
+package august.aug15_21;
+
+public class Card {
+}
+
