f39ca9f89eedde40a105af2e363109278022e18a
kshitij varshney <kshitijvarshney92@gmail.com>
2021-08-15 13:27:43 +0530

    final commit

--- src/august/aug15_21/Solution.java
+++ src/august/aug15_21/Solution.java
@@ -7,6 +7,34 @@
 
 package august.aug15_21;
 
+import java.util.Scanner;
+
 public class Solution {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while (t-- > 0) {
+            int n = sc.nextInt();
+            int[] arr = new int[n];
+            for (int i = 0; i < n; i++) {
+                arr[i] = sc.nextInt();
+            }
+            int k = sc.nextInt();
+            System.out.println(card(arr, n, k));
+        }
+    }
+
+    private static int card(int[] arr, int n, int k) {
+        int res = 0;
+        for (int start = n - k, i = start, win = 0; i < n + k; ++i) {
+            win += arr[i % n];
+            if (i - start >= k) {
+                win -= arr[(i - k) % n];
+            }
+            res = Math.max(win, res);
+        }
+        return res;
+    }
+
 }
 
--- src/august/aug15_21/TestCaseGen.java
+++ src/august/aug15_21/TestCaseGen.java
@@ -7,6 +7,37 @@
 
 package august.aug15_21;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+
 public class TestCaseGen {
+    public static void main(String[] args) throws IOException {
+        File file = new File("C:\\Users\\kshitij varshney\\IdeaProjects\\FT\\src\\august\\aug15_21\\input2.txt");
+        FileWriter fw = new FileWriter(file);
+        PrintWriter pw = new PrintWriter(fw);
+        int minT = 1;
+        int maxT = 1000;
+        int rangeT = maxT - minT + 1;
+        int t = (int) (Math.random() * rangeT) + minT;
+        pw.println(t);
+        for (int i = 0; i < t; i++) {
+            int minN = 5;
+            int maxN = 100;
+            int rangeN = maxN - minN + 1;
+            int n = (int) (Math.random() * rangeN) + minN;
+            pw.println(n);
+            for (int i1 = 0; i1 < n; i1++) {
+                int ele = (int) (Math.random() * 100) + 1;
+                pw.print(ele+" ");
+            }
+            pw.println();
+            int k = (int) (Math.random() * n-5) + 1;
+            pw.println(k);
+        }
+        pw.close();
+
+    }
 }
 
