b345f41950f3723e42f4d759eb00f0d0b9d36708
kshitij varshney <kshitijvarshney92@gmail.com>
2021-08-16 10:09:14 +0530

    final commit

--- src/august/aug16_21/TestCaseGen.java
+++ src/august/aug16_21/TestCaseGen.java
@@ -7,6 +7,43 @@
 
 package august.aug16_21;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Random;
+
 public class TestCaseGen {
+    public static void main(String[] args) throws IOException {
+        File file = new File("C:\\Users\\kshitij varshney\\IdeaProjects\\FT\\src\\august\\aug16_21\\input2.txt");
+        FileWriter fw = new FileWriter(file);
+        PrintWriter pw = new PrintWriter(fw);
+        int minT = 1;
+        int maxT = 500;
+        int rangeT = maxT - minT + 1;
+        int t = (int) (Math.random() * rangeT) + minT;
+        pw.println(t);
+        for (int i = 0; i < t; i++) {
+            int minN = 5;
+            int maxN = 50;
+            int rangeN = maxN - minN + 1;
+            int n = (int) (Math.random() * rangeN) + minN;
+            pw.println(n);
+            int len = (int) (Math.random() * 10) + 3;
+            for (int i1 = 0; i1 < n; i1++) {
+                for (int j = 0; j < len; j++) {
+                    Random random = new Random();
+                    boolean res = random.nextBoolean();
+                    pw.print(res == true ? 1 : 0 );
+                }
+                pw.println();
+            }
+            pw.println();
+        }
+        pw.close();
+
+    }
 }
+
+
 
