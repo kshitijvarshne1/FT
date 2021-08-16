809febabba3bd718ea77b127da9e2c0c94c27575
kshitij varshney <kshitijvarshney92@gmail.com>
2021-08-16 19:18:35 +0530

    create Ques2.java

---
+++ src/august/aug17_21/Ques2.java
@@ -0,0 +1,12 @@
+/*   Created by IntelliJ IDEA.
+ *   Author: Kshitij Varshney (kshitijvarshne1)
+ *   Date: 16-Aug-21
+ *   Time: 7:18 PM
+ *   File: Ques2.java
+ */
+
+package august.aug17_21;
+
+public class Ques2 {
+}
+
