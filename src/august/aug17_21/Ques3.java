b24087991b31aa13f0e0e26ea5bb1c6eb5af9716
kshitij varshney <kshitijvarshney92@gmail.com>
2021-08-16 19:32:53 +0530

    final commit

--- src/august/aug17_21/Ques3.java
+++ src/august/aug17_21/Ques3.java
@@ -8,5 +8,28 @@
 package august.aug17_21;
 
 public class Ques3 {
+    public static void main(String[] args) {
+        Count count = new Count();
+        String[] names = {"abe", "babe", "strobe"};
+
+        Integer[] intObjs = {1, 6, 3, 6, 5, 6, 7, 8};
+        System.out.println(count.count(names, "strobe"));
+        System.out.println(count.count(intObjs, 6));
+
+
+    }
+
+    static class Count {
+        public int count(Object[] arr, Object obj) {
+            int count = 0;
+            for (Object o : arr) {
+                if (o.equals(obj)) {
+                    count++;
+                }
+            }
+
+            return count;
+        }
+    }
 }
 
