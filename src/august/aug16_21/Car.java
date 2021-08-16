44139c211cdf3b4a7f0e7eb8af47095812ed1e84
kshitij varshney <kshitijvarshney92@gmail.com>
2021-08-16 10:09:46 +0530

    create Car.java

---
+++ src/august/aug16_21/Car.java
@@ -0,0 +1,12 @@
+/*   Created by IntelliJ IDEA.
+ *   Author: Kshitij Varshney (kshitijvarshne1)
+ *   Date: 16-Aug-21
+ *   Time: 10:09 AM
+ *   File: Car.java
+ */
+
+package august.aug16_21;
+
+public class Car {
+}
+
