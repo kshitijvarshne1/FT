9bc4cf3f118c0be592f5ac3b5c37e37cf377ce25
kshitij varshney <kshitijvarshney92@gmail.com>
2021-08-16 19:27:03 +0530

    final commit

--- src/august/aug17_21/Ques2.java
+++ src/august/aug17_21/Ques2.java
@@ -7,6 +7,31 @@
 
 package august.aug17_21;
 
+import java.util.HashMap;
+import java.util.Scanner;
+
 public class Ques2 {
+    public static void main(String[] args) {
+        System.out.println("Enter a word: ");
+        String word = new Scanner(System.in).next();
+        String vowelNumber = "";
+        HashMap<Character, Character> mp = new HashMap<>();
+        mp.put('a', '1');
+        mp.put('e', '2');
+        mp.put('i', '3');
+        mp.put('o', '4');
+        mp.put('u', '5');
+        for (int i = 0; i < word.length(); i++) {
+            String ch = String.valueOf(word.charAt(i));
+            ch = ch.toLowerCase();
+            if (mp.containsKey(ch.charAt(0))) {
+                vowelNumber += mp.get(ch.charAt(0));
+            } else {
+                vowelNumber += word.charAt(i);
+            }
+        }
+
+        System.out.println("VowelNumbered is: " + vowelNumber + " :");
+    }
 }
 
