81815ff805d7d50ba74810eb864f39e6fa82a3f6
kshitij varshney <kshitijvarshney92@gmail.com>
2021-08-23 15:00:56 +0530

    final commit

--- src/august/aug23_21/Calculator.java
+++ src/august/aug23_21/Calculator.java
@@ -7,34 +7,112 @@
 
 package august.aug23_21;
 
+import java.io.IOException;
 import java.util.Scanner;
 
 public class Calculator {
     public static void main(String[] args) {
+        String[] operations = {"", "addition", "subtraction", "multiply", "division"};
         Scanner sc = new Scanner(System.in);
         System.out.println("Welcome to John Doe Handy Calculator");
-        showOptions();
-        int option =0;
-        while (true){
-            String inp = sc.next();
-            try{
-                option= Integer.parseInt(inp);
+
+        while (true) {
+            // show calculator options
+            showOptions();
+            int option = 0;
+            boolean repeat1 = true;
+            // input for option
+            while (repeat1) {
+                try {
+                    option = sc.nextInt();
+                    repeat1 = false;
+                } catch (Exception e) {
+                    System.out.println("You have entered an invalid choice, please re-enter your choice:");
+                    sc.next();
+                    repeat1 = true;
+                }
             }
-            catch (Exception e){
-                System.out.println("You have entered an invalid choice, please re-enter your choice:");
+            float val1 = 0.0f;
+            float val2 = 0.0f;
+            if (option <= 4) {
+                System.out.println("Please enter two float number to " + operations[option] + " separated by space");
+
+                // input for value 1 for operation
+                boolean repeat2 = true;
+                while (repeat2) {
+                    try {
+                        val1 = sc.nextFloat();
+                        repeat2 = false;
+                    } catch (Exception e) {
+                        System.out.println("You have entered wrong format value");
+                        sc.next();
+                        repeat2 = true;
+                    }
+                }
+                boolean repeat3 = true;
+                // input for value 3 for operation
+                while (repeat3) {
+
+                    try {
+                        val2 = sc.nextFloat();
+                        repeat3 = false;
+                    } catch (Exception e) {
+                        System.out.println("You have entered wrong format value");
+                        sc.next();
+                        repeat3 = true;
+                    }
+                }
+                // if value 2 is 0.0 and operation is division
+                // again input for both values
+                if (operations[option].equalsIgnoreCase("division") && val2 == 0.0) {
+                    System.out.println("You have entered invalid floats please re -enter");
+                    repeat2 = true;
+                    while (repeat2) {
+                        try {
+                            val1 = sc.nextFloat();
+                            repeat2 = false;
+                        } catch (Exception e) {
+                            System.out.println("You have entered wrong format value");
+                            sc.next();
+                            repeat2 = true;
+                        }
+                    }
+                    repeat3 = true;
+
+                    while (repeat3) {
+
+                        try {
+                            val2 = sc.nextFloat();
+                            repeat3 = val2 == 0.0;
+                        } catch (Exception e) {
+                            System.out.println("You have entered wrong format value");
+                            sc.next();
+                            repeat3 = true;
+                        }
+                    }
+                }
             }
-            switch (option){
+            // distinguish the option according to requirement
+            switch (option) {
                 case 1:
-                    System.out.println("Please enter two float number to addition separated by space");
+                    float result1 = val1 + val2;
+                    System.out.print("Result of addition of " + val1 + "  and " + val2 + " is ");
+                    System.out.printf("%.2f\n", result1);
                     break;
                 case 2:
-                    System.out.println("Please enter two float number to subtraction separated by space");
+                    float result2 = val1 - val2;
+                    System.out.print("Result of subtraction from " + val1 + "  to " + val2 + " is ");
+                    System.out.printf("%.2f\n", result2);
                     break;
                 case 3:
-                    System.out.println("Please enter two float number to multiply separated by space");
+                    float result3 = val1 * val2;
+                    System.out.print("Result of multiply of " + val1 + "  and " + val2 + " is ");
+                    System.out.printf("%.2f\n", result3);
                     break;
                 case 4:
-                    System.out.println("Please enter two float number to division separated by space");
+                    float result4 = val1 / val2;
+                    System.out.print("Result of division of " + val1 + "  and " + val2 + " is ");
+                    System.out.printf("%.2f\n", result4);
                     break;
                 case 5:
                     System.out.println("Thank you for using John Doe Handy Calculator");
@@ -42,9 +120,16 @@ public class Calculator {
                 default:
                     System.out.println("please enter the correct input");
             }
+            System.out.println("Press enter to continue");
+            try {
+                System.in.read();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
     }
 
+    // method to create show options
     public static void showOptions() {
         System.out.println("1. Addition \n" +
                 "2. Subtraction \n" +
