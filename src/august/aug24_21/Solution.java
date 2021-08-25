274a9271d904832c67f9436a21dcecb5da3ed117
kshitij varshney <kshitijvarshney92@gmail.com>
2021-08-25 19:51:31 +0530

    final commit

--- src/august/aug24_21/Solution.java
+++ src/august/aug24_21/Solution.java
@@ -12,12 +12,15 @@ import java.util.Scanner;
 public class Solution {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int[] a = new int[n];
-        for (int i = 0; i < n; i++) {
-            a[i] = sc.nextInt();
+        int t = sc.nextInt();
+        while (t-- > 0) {
+            int n = sc.nextInt();
+            int[] a = new int[n];
+            for (int i = 0; i < n; i++) {
+                a[i] = sc.nextInt();
+            }
+            System.out.println(solve(a));
         }
-        System.out.println(solve(a));
     }
 
     private static int solve(int[] nums) {
