790cbe6acfdb3d6d68b29f0a37b4c03313b07f0d
kshitij varshney <kshitijvarshney92@gmail.com>
2021-08-26 17:09:49 +0530

    final commit

--- src/august/aug26_21/FizzBuzz.java
+++ src/august/aug26_21/FizzBuzz.java
@@ -7,9 +7,38 @@
 
 package august.aug26_21;
 
+import java.util.Scanner;
+
 public class FizzBuzz {
     public static void main(String[] args) {
-
+        // create a object of Scanner class for input the data from the console
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number :- ");
+        // Number till you want to print the FizzBuzz
+        int n = sc.nextInt();
+        //for loop executes until the condition i<=n becomes false
+        for (int i = 1; i < n; i++) {
+            //returns true if both the conditions return true
+            if (i % 3 == 0 && i % 5 == 0) {
+                //prints if the number is multiple of 3 and 5
+                System.out.print("FizzBuzz");
+            }
+            //executes if the condition returns true
+            else if (i % 3 == 0) {
+                //prints if the number is multiple of 3
+                System.out.print("Fizz");
+            }
+            //executes if the condition returns true
+            else if (i % 5 == 0) {
+                //prints if the number is multiple of 5
+                System.out.print("Buzz");
+            } else {
+                //prints the number itself if the number is not divisible by both 3 and 5
+                System.out.print(i);
+            }
+            //prints space
+            System.out.print("," + " ");
+        }
     }
 }
 
