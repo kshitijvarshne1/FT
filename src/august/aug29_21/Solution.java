9164f08721b6ae562c2386c64e2f34af9eb9c696
kshitij varshney <kshitijvarshney92@gmail.com>
2021-08-29 14:10:11 +0530

    final commit

--- src/august/aug29_21/Solution.java
+++ src/august/aug29_21/Solution.java
@@ -43,23 +43,22 @@ public class Solution {
         }
 
         private void dfs(int taskID, int sessionCount) {
-            if (sessionCount > res) return; //prune, if we didn't use prune, it will be 2200ms, if lucky you get ac
+            if (sessionCount > res) return;
             if (taskID == tasks.length) {
                 res = Math.min(res, sessionCount);
                 return;
             }
             for (int i = 0; i < sessionCount; i++)
-                if (sessions[i] + tasks[taskID] <= maxSessionTime) { //put task into old session bucket
+                if (sessions[i] + tasks[taskID] <= maxSessionTime) {
                     sessions[i] += tasks[taskID];
                     dfs(taskID + 1, sessionCount);
                     sessions[i] -= tasks[taskID];
                 }
-            sessions[sessionCount] += tasks[taskID]; //put task into new empty session bucket
+            sessions[sessionCount] += tasks[taskID];
             dfs(taskID + 1, sessionCount + 1);
             sessions[sessionCount] -= tasks[taskID];
         }
 
-        //optional, it just speed up from 68ms to 1ms
         private void reverseSort(int[] tasks) {
             Arrays.sort(tasks);
             int[] a = new int[tasks.length];
