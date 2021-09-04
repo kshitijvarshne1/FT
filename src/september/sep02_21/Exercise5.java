4bbc2e5b39d07f7a103f14227911a0a4179569e5
kshitij varshney <kshitijvarshney92@gmail.com>
2021-09-04 12:35:16 +0530

    add processInputOutputFile method

--- src/september/sep02_21/Exercise5.java
+++ src/september/sep02_21/Exercise5.java
@@ -7,10 +7,18 @@
 
 package september.sep02_21;
 
+import jdk.nashorn.internal.runtime.regexp.joni.ast.StringNode;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Scanner;
+
 public class Exercise5 {
 
     public static void main(String[] args) {
         processCLArgument(args);
+        processInputOutputFile(args);
     }
 
     public static void processCLArgument(String[] args) {
@@ -20,6 +28,38 @@ public class Exercise5 {
         } else {
             System.out.println("Input will be read From: " + args[0] + "\nOutput will be written into: " + args[1]);
             System.out.println();
+        }
+    }
+    public static void processInputOutputFile(String[] args){
+        String path ="C:\\Users\\kshitij varshney\\IdeaProjects\\FT\\src\\september\\sep02_21\\";
+        File inputFile = new File(path+args[0]);
+        if(!inputFile.exists()){
+            System.out.println("Input file "+args[0] +" does not exist");
+            System.exit(2);
+        }
+        //Output File
+        File outputFile = new File(path+args[1]);
+        try{
+            Scanner input = new Scanner(inputFile);
+            PrintWriter output = new PrintWriter(outputFile);
+            int i=1;
+            while (input.hasNextLine()){
+                String line = input.nextLine();
+                System.out.println("Student # "+i+" "+line);
+                String [] studentInfo= line.split(",");
+                output.println("Student # "+i++ +" "+" is:\" "+studentInfo[0]+"\" whose raw scores are :");
+                for(int j=1;j<studentInfo.length;j++){
+                    output.print(studentInfo[j]+":  ");
+                }
+                output.println();
+            }
+            input.close();
+            output.close();
+
+        }
+
+        catch (IOException e){
+            System.out.println("Error reading from input file "+args[0]);
         }
     }
 }
