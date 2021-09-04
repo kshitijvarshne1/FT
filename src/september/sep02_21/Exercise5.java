87cdf5b9c0eb083d06387b172b6ebc4a93254a83
kshitij varshney <kshitijvarshney92@gmail.com>
2021-09-04 11:33:03 +0530

    create Exercise5.java

---
+++ src/september/sep02_21/Exercise5.java
@@ -0,0 +1,12 @@
+/*   Created by IntelliJ IDEA.
+ *   Author: Kshitij Varshney (kshitijvarshne1)
+ *   Date: 04-Sep-21
+ *   Time: 11:30 AM
+ *   File: Exercise5.java
+ */
+
+package september.sep02_21;
+
+public class Exercise5 {
+}
+
