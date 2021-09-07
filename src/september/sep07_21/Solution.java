f190117a39218d3424b7999613ff18f238790647
kshitij varshney <kshitijvarshney92@gmail.com>
2021-09-07 19:02:30 +0530

    final commit

--- src/september/sep07_21/Solution.java
+++ src/september/sep07_21/Solution.java
@@ -7,6 +7,28 @@
 
 package september.sep07_21;
 
+import java.util.Scanner;
+
 public class Solution {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while (t-- > 0) {
+            int x = sc.nextInt();
+            System.out.println(solve(x));
+        }
+    }
+
+    private static String solve(int n) {
+        int a = 0, b = 0;
+        String s = String.valueOf(n);
+        for (int i = 0; i < s.length(); i++) {
+            if (i % 2 != 0)
+                a = a * 10 + s.charAt(i) - '0';
+            else
+                b = b * 10 + s.charAt(i) - '0';
+        }
+        return  String.valueOf((a + 1) * (b + 1) - 2);
+    }
 }
 
