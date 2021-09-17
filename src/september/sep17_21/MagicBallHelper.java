ce7d26d992f70bcc2ec2d3a44b73b5f8fdb88ae3
kshitij varshney <kshitijvarshney92@gmail.com>
2021-09-17 11:23:53 +0530

    create MagicBallHelper.java

---
+++ src/september/sep17_21/MagicBallHelper.java
@@ -0,0 +1,12 @@
+/*   Created by IntelliJ IDEA.
+ *   Author: Kshitij Varshney (kshitijvarshne1)
+ *   Date: 17-Sep-21
+ *   Time: 11:23 AM
+ *   File: MagicBallHelper.java
+ */
+
+package september.sep17_21;
+
+public class MagicBallHelper {
+}
+
