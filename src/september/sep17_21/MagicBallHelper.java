d0a4f94b69e5e316f07a8b54dac102f19da7f626
kshitij varshney <kshitijvarshney92@gmail.com>
2021-09-17 11:31:56 +0530

    final commit

--- src/september/sep17_21/MagicBallHelper.java
+++ src/september/sep17_21/MagicBallHelper.java
@@ -7,6 +7,37 @@
 
 package september.sep17_21;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class MagicBallHelper {
+    public static void main(String[] args) throws FileNotFoundException {
+        String[] ans = new String[1000];
+        int count = readAnswers(ans);
+        for (int i = 0; i < count; i++) {
+            System.out.println(ans[i]);
+        }
+        System.out.println(count);
+
+    }
+
+    public static int readAnswers(String[] answers) {
+        int count = 0;
+        Scanner file = null;
+        try {
+            file = new Scanner(new File("C:\\Users\\kshitij varshney\\IdeaProjects\\FT\\src\\september\\sep17_21\\answer.txt"));
+            while (file.hasNextLine()) {
+                String line = file.nextLine();
+                answers[count] = line;
+                count++;
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println("Error: Data file Not found");
+        }
+
+
+        return count;
+    }
 }
 
