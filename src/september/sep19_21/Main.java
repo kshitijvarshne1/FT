a1c156be4a7ffc7c7f2d13823eb57a3ee49117b8
kshitij varshney <kshitijvarshney92@gmail.com>
2021-09-19 10:22:01 +0530

    add basic structure for the Theater

--- src/september/sep19_21/Main.java
+++ src/september/sep19_21/Main.java
@@ -7,9 +7,36 @@
 
 package september.sep19_21;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number of row and number of seats in one row");
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        System.out.println("Enter the theater name");
+        String tName = sc.next();
+        Theater theater = new Theater(n, m, tName);
+    }
 
+}
+
+class Theater {
+    private final double[][] seats;
+    private final String eventName;
+
+    public Theater(int n, int m, String e) {
+        this.seats = new double[n][m];
+        this.eventName = e;
+    }
+
+    public double[][] getSeats() {
+        return seats;
+    }
+
+    public String getEventName() {
+        return eventName;
     }
 }
 
