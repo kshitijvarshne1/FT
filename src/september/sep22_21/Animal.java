8300e5f7e07c5825f731d7547cd9015423569cde
kshitij varshney <kshitijvarshney92@gmail.com>
2021-09-22 17:51:06 +0530

    final commit

--- src/september/sep22_21/Animal.java
+++ src/september/sep22_21/Animal.java
@@ -19,5 +19,52 @@ public class Animal {
         this.age = age;
         this.training = training;
     }
+
+    public Animal(String name, String type) {
+        this.name = name;
+        this.type = type;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public boolean isTraining() {
+        return training;
+    }
+
+    public void setTraining(boolean training) {
+        this.training = training;
+    }
+
+    @Override
+    public String toString() {
+        return "Animal{" +
+                "name='" + name + '\'' +
+                ", type='" + type + '\'' +
+                ", age=" + age +
+                ", training=" + training +
+                '}' + "\n";
+    }
 }
 
