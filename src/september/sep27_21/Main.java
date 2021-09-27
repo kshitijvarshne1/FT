688b693b949b95a23cd5e34f12e31991c2039f04
kshitij varshney <kshitijvarshney92@gmail.com>
2021-09-27 16:07:36 +0530

    final commit

--- src/september/sep27_21/Main.java
+++ src/september/sep27_21/Main.java
@@ -10,7 +10,6 @@ package september.sep27_21;
 public class Main {
     public static void main(String[] args) {
         Calender calender= new Calender();
-
         calender.printCalender();
     }
 }
