8b4962c948639697c9e59e57aa76e54973860cdc
kshitij varshney <kshitijvarshney92@gmail.com>
2021-09-27 15:39:30 +0530

    create Data.java

---
+++ src/september/sep27_21/Date.java
@@ -0,0 +1,12 @@
+/*   Created by IntelliJ IDEA.
+ *   Author: Kshitij Varshney (kshitijvarshne1)
+ *   Date: 27-Sep-21
+ *   Time: 3:39 PM
+ *   File: Data.java
+ */
+
+package september.sep27_21;
+
+public class Date {
+}
+
