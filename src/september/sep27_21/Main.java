698df7cd8f9791ea784a191ccfc4860d18c5aec9
kshitij varshney <kshitijvarshney92@gmail.com>
2021-09-27 16:02:55 +0530

    create Main.java

---
+++ src/september/sep27_21/Main.java
@@ -0,0 +1,12 @@
+/*   Created by IntelliJ IDEA.
+ *   Author: Kshitij Varshney (kshitijvarshne1)
+ *   Date: 27-Sep-21
+ *   Time: 4:02 PM
+ *   File: Main.java
+ */
+
+package september.sep27_21;
+
+public class Main {
+}
+
