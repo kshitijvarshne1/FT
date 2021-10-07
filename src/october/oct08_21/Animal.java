16d867cf55f90517433d8d1e7489fdad7442db29
kshitij varshney <kshitijvarshney92@gmail.com>
2021-10-08 00:47:02 +0530

    final commit

--- src/october/oct08_21/Animal.java
+++ src/october/oct08_21/Animal.java
@@ -17,5 +17,29 @@ public class Animal {
         this.breed = breed;
         this.age = age;
     }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getBreed() {
+        return breed;
+    }
+
+    public void setBreed(String breed) {
+        this.breed = breed;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
 }
 
