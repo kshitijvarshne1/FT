3b69cd2dc25c39ed856abc1df84824a59e22ba3f
kshitij varshney <kshitijvarshney92@gmail.com>
2021-10-08 00:46:46 +0530

    add constructor and instance variable

--- src/october/oct08_21/Animal.java
+++ src/october/oct08_21/Animal.java
@@ -8,5 +8,14 @@
 package october.oct08_21;
 
 public class Animal {
+    private String name;
+    private String breed;
+    private int age;
+
+    public Animal(String name, String breed, int age) {
+        this.name = name;
+        this.breed = breed;
+        this.age = age;
+    }
 }
 
