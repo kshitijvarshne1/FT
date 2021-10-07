a3df62d22b238dfd56e0f2685c8ef3228065f8f8
kshitij varshney <kshitijvarshney92@gmail.com>
2021-10-08 00:46:05 +0530

    create Animal.java

---
+++ src/october/oct08_21/Animal.java
@@ -0,0 +1,12 @@
+/*   Created by IntelliJ IDEA.
+ *   Author: Kshitij Varshney (kshitijvarshne1)
+ *   Date: 08-Oct-21
+ *   Time: 12:45 AM
+ *   File: Animal.java
+ */
+
+package october.oct08_21;
+
+public class Animal {
+}
+
