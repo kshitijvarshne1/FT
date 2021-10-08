46cb68c66afee443ab53f90483f64633efa936c5
kshitij varshney <kshitijvarshney92@gmail.com>
2021-10-08 12:05:18 +0530

    final commit

--- src/october/oct08_21/Main.java
+++ src/october/oct08_21/Main.java
@@ -7,21 +7,28 @@
 
 package october.oct08_21;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
+import java.io.*;
+import java.util.ArrayList;
 
 public class Main {
-    public static void main(String[] args) throws FileNotFoundException {
+    public static void main(String[] args) throws IOException {
         // get the file path
         File file = new File("C:\\Users\\kshitij varshney\\IdeaProjects\\FT\\src\\october\\oct08_21\\mypet.txt");
         // Creating an object of BuffferedReader class
+        BufferedReader br = null;
         try {
-            BufferedReader br = new BufferedReader(new FileReader(file));
+            br = new BufferedReader(new FileReader(file));
         } catch (FileNotFoundException e) {
             System.out.println("File not found");
         }
+        // creating arraylist of Dog type
+        ArrayList<Dog> dogs= new ArrayList<>();
+        PrintWriter pWirter =null;
+        pWirter = new PrintWriter(new File("D:\\testout.txt"));
+        writer1.close();
+        String st;
+        while ((st = br.readLine()) != null)
+
     }
 }
 
