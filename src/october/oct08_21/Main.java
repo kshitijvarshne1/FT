21b6f1f2b7aa5e04ba1db2e1612929f5a45db12c
kshitij varshney <kshitijvarshney92@gmail.com>
2021-10-08 12:01:12 +0530

    final commit

--- src/october/oct08_21/Main.java
+++ src/october/oct08_21/Main.java
@@ -7,6 +7,21 @@
 
 package october.oct08_21;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+
 public class Main {
+    public static void main(String[] args) throws FileNotFoundException {
+        // get the file path
+        File file = new File("C:\\Users\\kshitij varshney\\IdeaProjects\\FT\\src\\october\\oct08_21\\mypet.txt");
+        // Creating an object of BuffferedReader class
+        try {
+            BufferedReader br = new BufferedReader(new FileReader(file));
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found");
+        }
+    }
 }
 
