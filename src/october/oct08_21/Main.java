8a9f76c8f22ed47db9a1396e220be374e3c780ff
kshitij varshney <kshitijvarshney92@gmail.com>
2021-10-08 11:51:02 +0530

    create Main.java and input.txt

---
+++ src/october/oct08_21/Main.java
@@ -0,0 +1,12 @@
+/*   Created by IntelliJ IDEA.
+ *   Author: Kshitij Varshney (kshitijvarshne1)
+ *   Date: 08-Oct-21
+ *   Time: 11:50 AM
+ *   File: Main.java
+ */
+
+package october.oct08_21;
+
+public class Main {
+}
+
