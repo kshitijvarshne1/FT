5a111fe05f3e00e1fba57d13463842b33df9a28c
kshitij varshney <kshitijvarshney92@gmail.com>
2021-10-11 18:32:16 +0530

    add getter and setter

--- src/october/oct12_21/Media.java
+++ src/october/oct12_21/Media.java
@@ -8,14 +8,43 @@
 package october.oct12_21;
 
 public abstract class Media {
-    private int id;
+    // instance variable
+    private final int id;
     private String title;
     private int yearPublished;
 
+    // constructor
     public Media(int id, String title, int yearPublished) {
         this.id = id;
         this.title = title;
         this.yearPublished = yearPublished;
+    }
+
+    // getter and setter methods
+    public int getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public int getYearPublished() {
+        return yearPublished;
+    }
+
+    public void setYearPublished(int yearPublished) {
+        this.yearPublished = yearPublished;
+    }
+
+
+    // calculate rental fee; for generic media it is flat fee $1.50
+    public double calculateRentalFee() {
+        return 1.50;
     }
 }
 
