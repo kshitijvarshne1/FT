3f05153cb9fabf8c5da3e7a12467c98cd9a78aaa
kshitij varshney <kshitijvarshney92@gmail.com>
2021-10-11 18:26:46 +0530

    create Media.java

---
+++ src/october/oct12_21/Media.java
@@ -0,0 +1,12 @@
+/*   Created by IntelliJ IDEA.
+ *   Author: Kshitij Varshney (kshitijvarshne1)
+ *   Date: 11-Oct-21
+ *   Time: 6:26 PM
+ *   File: Media.java
+ */
+
+package october.oct12_21;
+
+public class Media {
+}
+
