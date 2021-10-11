3dd0c508db8e0319cd48570dfd29d9684d541a6f
kshitij varshney <kshitijvarshney92@gmail.com>
2021-10-11 19:05:02 +0530

    create Manager.java

---
+++ src/october/oct12_21/Manager.java
@@ -0,0 +1,12 @@
+/*   Created by IntelliJ IDEA.
+ *   Author: Kshitij Varshney (kshitijvarshne1)
+ *   Date: 11-Oct-21
+ *   Time: 7:04 PM
+ *   File: Manager.java
+ */
+
+package october.oct12_21;
+
+public class Manager {
+}
+
