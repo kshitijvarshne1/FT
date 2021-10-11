2447ac076ec9e55cf67c6ade2961d0aa0738ae3d
kshitij varshney <kshitijvarshney92@gmail.com>
2021-10-11 18:38:48 +0530

    add calculateRentalFee method

--- src/october/oct12_21/EBook.java
+++ src/october/oct12_21/EBook.java
@@ -7,12 +7,43 @@
 
 package october.oct12_21;
 
-public class EBook extends Media{
+import java.util.Calendar;
+
+public class EBook extends Media {
+    // instance variable
     private int numberOfChapter;
 
+    // constructor
     public EBook(int id, String title, int yearPublished, int numberOfChapter) {
         super(id, title, yearPublished);
         this.numberOfChapter = numberOfChapter;
+    }
+
+    // getter and setter
+    public int getNumberOfChapter() {
+        return numberOfChapter;
+    }
+
+    public void setNumberOfChapter(int numberOfChapter) {
+        this.numberOfChapter = numberOfChapter;
+    }
+
+    @Override
+    public String toString() {
+        return "EBook{" +
+                "numberOfChapter=" + numberOfChapter +
+                '}';
+    }
+
+    // calculateRentalFee method
+    public double calculateRentalFee() {
+        double fee = this.numberOfChapter * 0.10 + 1; // basic fee
+        int currYear = Calendar.getInstance().get(Calendar.YEAR);
+        // add $1.00 fee if current year is same as published year
+        if (this.getYearPublished() == currYear) {
+            fee += 1;
+        }
+        return fee;
     }
 }
 
