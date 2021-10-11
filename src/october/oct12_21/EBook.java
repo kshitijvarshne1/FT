854644f9ecf24e4cf017c1c87209cc4d9a145aaa
kshitij varshney <kshitijvarshney92@gmail.com>
2021-10-11 18:32:49 +0530

    create EBook.java

---
+++ src/october/oct12_21/EBook.java
@@ -0,0 +1,12 @@
+/*   Created by IntelliJ IDEA.
+ *   Author: Kshitij Varshney (kshitijvarshne1)
+ *   Date: 11-Oct-21
+ *   Time: 6:32 PM
+ *   File: EBook.java
+ */
+
+package october.oct12_21;
+
+public class EBook {
+}
+
