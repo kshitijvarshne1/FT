59416a25c245be1872808bd07c38f92a89c1b15f
kshitij varshney <kshitijvarshney92@gmail.com>
2021-10-11 19:25:10 +0530

    final commit

--- src/october/oct12_21/Manager.java
+++ src/october/oct12_21/Manager.java
@@ -7,6 +7,28 @@
 
 package october.oct12_21;
 
+import java.io.*;
+import java.util.ArrayList;
+
 public class Manager {
+    private final ArrayList<Media> mediaArrayList;
+
+    public Manager() {
+        mediaArrayList = new ArrayList<>();
+    }
+
+    // method to load media object
+    public void loadMediaObject(Media media) throws IOException {
+        mediaArrayList.add(media);
+    }
+
+    // method to show the media data
+    public void showData() {
+        for (Media media : mediaArrayList) {
+            System.out.println(media.toString());
+        }
+    }
 }
+
+
 
