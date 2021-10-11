147935cced5ac69ef9e6a384b7ac9fefd33a7fb5
kshitij varshney <kshitijvarshney92@gmail.com>
2021-10-11 18:42:10 +0530

    create MusicCD.java

---
+++ src/october/oct12_21/MusicCD.java
@@ -0,0 +1,12 @@
+/*   Created by IntelliJ IDEA.
+ *   Author: Kshitij Varshney (kshitijvarshne1)
+ *   Date: 11-Oct-21
+ *   Time: 6:41 PM
+ *   File: MusicCD.java
+ */
+
+package october.oct12_21;
+
+public class MusicCD {
+}
+
